f39d7c2f5970b6f05bce838681c328c9ccd7eed1
Sushant Shah <blckblze@gmail.com>
2022-03-15 13:11:15 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from re import template
+import CyroWebB, jwt
+
+server = CyroWebB.Server(__name__)
+template = CyroWebB.Template(server)
+
+@server.create_endpoint("/")
+def index(request: CyroWebB.Request.request, response: CyroWebB.Response.response):
+    response.text = template("index.jinja")
