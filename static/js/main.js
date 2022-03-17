5156a75dbd35890d66c1de330edcdff6940e448a
Sushant Shah <blckblze@gmail.com>
2022-03-17 19:16:08 +0530

    Completed Auth Flow

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,44 @@ import { createUser, authenticateUser, signOutUser } from "/js/authentication.js
 
 var user, errorCode, errorMessage;
 
+document.querySelector("#account").addEventListener("click", function() {
+    if (user == null || user == undefined || user == "") {
+        document.querySelector("#signup").classList.add("active");
+    } else {
+        document.location.href = "/profile";
+    }
+});
+
+document.querySelector("#signup").addEventListener("click", function() {
+    document.querySelector("#signup").classList.remove("active");
+});
+
+document.querySelector("#signup-form").addEventListener("click", function(e) {
+    e.stopPropagation();
+});
+
+document.querySelector("#login-form").addEventListener("submit", function(e) {
+    e.stopPropagation();
+});
+
+document.querySelector("#login").addEventListener("click", function() {
+    document.querySelector("#login").classList.remove("active");
+});
+
+document.querySelectorAll(".to-signup").forEach((element) => {
+    element.addEventListener("click", function() {
+        document.querySelector("#login").classList.remove("active");
+        document.querySelector("#signup").classList.add("active");
+    });
+});
+
+document.querySelectorAll(".to-login").forEach((element) => {
+    element.addEventListener("click", function() {
+        document.querySelector("#signup").classList.remove("active");
+        document.querySelector("#login").classList.add("active");
+    });
+});
+
 document.querySelector("#signup-form").addEventListener("submit", (e) => {
     e.preventDefault();
     const email = document.querySelector("#signup-email").value;
