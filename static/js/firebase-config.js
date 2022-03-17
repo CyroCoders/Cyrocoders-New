c3a2aae3e76a44feed5b5fa8bd29131a59d3ed50
Sushant Shah <blckblze@gmail.com>
2022-03-17 17:44:48 +0530

    Added Firebase Config For Authentication And Firestore

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-analytics.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js"
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js"
 
 const app = initializeApp({
     apiKey: "AIzaSyBWSQN_POGcgiiRy_FS0oB62l26Uth9HGE",
@@ -11,4 +13,6 @@ const app = initializeApp({
     appId: "1:207854820836:web:2ddf94245fe00cce5e7bf1",
     measurementId: "G-NZEP3J40W2"
 });
-const analytics = getAnalytics(app);
+const db = getFirestore();
+
+export { app, db, collection, getDocs, getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut };
