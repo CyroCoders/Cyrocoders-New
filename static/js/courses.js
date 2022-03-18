316950d3945cb84544a50d687f40b249f3e329fd
Sushant Shah <blckblze@gmail.com>
2022-03-18 12:51:13 +0530

    Fixed Issues In Course/Lesson Fetch

--- static/js/courses.js
+++ static/js/courses.js
@@ -2,16 +2,16 @@ import { db, collection, getDocs } from "/js/firebase-config.js";
 var Courses = collection(db, "CourseList");
 
 var premiumCourses = [];
-var freeCourses = [];
+var freeLessons = [];
 
-getDocs(Courses).then((querySnapshot) => {
+await getDocs(Courses).then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         if(doc.data().premium == true) {
             premiumCourses.push(doc.data());
         } else {
-            freeCourses.push(doc.data());
+            freeLessons.push(doc.data());
         }
     });
 });
 
-export { premiumCourses, freeCourses };
+export { premiumCourses, freeLessons };
