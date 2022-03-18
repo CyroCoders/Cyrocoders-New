d2ef03553bfed2640baf60ab228788880d5a5f98
Sushant Shah <blckblze@gmail.com>
2022-03-18 20:29:30 +0530

    Added doc Class Import/Export

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-analytics.js";
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js"
+import { getFirestore, doc, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js"
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js"
 
 const app = initializeApp({
@@ -15,4 +15,4 @@ const app = initializeApp({
 });
 const db = getFirestore();
 
-export { app, db, collection, getDocs, getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut };
+export { app, db, doc, collection, getDocs, getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut };
