962c9a433b7ca49a64b30119f9655a8866ebb6ac
Sushant Shah <blckblze@gmail.com>
2022-03-18 15:02:05 +0530

    Added Profile Endpoint

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ template = CyroWebB.Template(server)
 @server.create_endpoint("/")
 def index(request: CyroWebB.Request.request, response: CyroWebB.Response.response):
     response.text = template("index.jinja")
+
+@server.create_endpoint("/profile")
+def index(request: CyroWebB.Request.request, response: CyroWebB.Response.response):
+    response.text = template("profile.jinja")
